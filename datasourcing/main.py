1956ed2039e6d4b8b2015cddd25cddb71337752f
smehta12 <shalinmehta85@gmail.com>
2024-06-30 11:05:51 -0700

    Added main to fetch and cleanup the data

---
+++ datasourcing/main.py
@@ -0,0 +1,29 @@
+import yaml
+
+from datasourcing.scraping import Scrapper
+from datasourcing.cleanup import Cleanup
+
+import logging
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+
+
+def data_fetching_main():
+    cuisine = "mh"
+    base_data_dir = f"../data/"
+
+    with open("./configs.yaml") as f:
+        configs = yaml.safe_load(f)
+    recipe_pages_info = configs["recipe_pages_info"]
+
+    s = Scrapper()
+    s.scrape(recipe_pages_info, cuisine, base_data_dir)
+
+    c = Cleanup(base_data_dir, cuisine, recipe_pages_info)
+    c.remove_tags_word_from_tags()
+    c.add_item_name_in_recipe_last_line()
+    c.find_lesser_line_of_ing_and_recipe()
+
+
+if __name__ == '__main__':
+    data_fetching_main()
